4c48ecc89d4f3ebe74cd074f1fcb6a45d49efe4a
fanglansheng <wind_7318@163.com>
2016-05-11 08:30:10 -0700

    changes to form

---
+++ webapps/recipe/views_home.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from recipe.forms import *
+
+# Create your views here.
+
+# home that can be accessed by all people even without logging in
+# @login_required
+def home(request):
+    context = {}
+    context['work_form'] = CreateWorkForm()
+    return render(request, 'recipe/index.html', context)
--- webapps/webapps/urls.py
+++ webapps/webapps/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^/', include('recipe.urls')),
+    url(r'^', include('recipe.urls')),
 ]
