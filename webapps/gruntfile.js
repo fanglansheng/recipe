f005dfa04ca5c9ace4cfc383eddcbba0f8f76235
fanglansheng <wind_7318@163.com>
2016-06-08 23:14:19 -0700

    add gruntfile

---
+++ webapps/gruntfile.js
@@ -0,0 +1,73 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Task configuration goes here.
+    sass: {
+      main: {
+        files: {
+          'recipe/static/recipe/css/work.css': 'recipe/static/recipe/css/work.scss'
+          'recipe/static/recipe/css/nav.css': 'recipe/static/recipe/css/nav.scss'
+        }
+      },
+      // deploy: {
+      //   options: {
+      //     includePaths: ['bower_components/foundation/scss'],
+      //     outputStyle: 'compressed'
+      //   },
+      //   files: {
+      //     'build/static/css/screen.min.css': 'myproject/static/scss/screen.scss'
+      //   }
+      // }
+    },
+
+    concurrent: {
+      dev: {
+        tasks: ['shell:django', 'watch'],
+        options: {
+          logConcurrentOutput: true
+        }
+      }
+    },
+
+    watch: {
+      sass: {
+        files: ['recipe/static/recipe/css/*.scss'],
+        tasks: ['sass'],
+      },
+    },
+
+    shell: {
+      options: {
+        stderr: false
+      },
+      django: {
+        command: 'python3 manage.py runserver',
+        options:{
+          stdout:true,
+          stderr:true,
+          stdin:true,
+        }
+      },
+    }
+  });
+
+  // Load plugins here.
+  // grunt.loadNpmTasks('grunt-contrib-concat');
+  // grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-concurrent');
+
+  // grunt.loadNpmTasks('grunt-contrib-less');
+  // grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Register tasks here.
+  grunt.registerTask('default', ['sass:main','concurrent']);
+
+};
